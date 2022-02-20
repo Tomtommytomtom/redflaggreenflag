2492a18610cd0b890482b3592e0be6d0237be6ca
Tom Pillokat <tom.pillokat@googlemail.com>
2022-02-20 13:11:19 +0100

    Updates

---
+++ assets/index.8811c194.js
@@ -0,0 +1,395 @@
+import{r as w,o as i,c as u,a as l,b as k,t as _,d as y,F as h,e as c,f as d,g as b,h as I,i as O}from"./vendor.6603041b.js";const x=function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const a of o.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&s(a)}).observe(document,{childList:!0,subtree:!0});function n(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerpolicy&&(o.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?o.credentials="include":e.crossorigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function s(e){if(e.ep)return;e.ep=!0;const o=n(e);fetch(e.href,o)}};x();var L=(r,t)=>{const n=r.__vccOpts||r;for(const[s,e]of t)n[s]=e;return n};const S={};function C(r,t){const n=w("router-view");return i(),u("div",null,[l("main",null,[k(n)])])}var H=L(S,[["render",C]]),A=`Fiercely loyal\r
+Believes you are the most attractive person they've ever seen\r
+Trusting\r
+Respectful\r
+Honest\r
+Patient\r
+Reliable\r
+Courteous\r
+Funniest person in the world\r
+Affectionate\r
+Good listener\r
+Supportive\r
+Generous\r
+Driven\r
+Perfect communicator\r
+Always makes time for you\r
+Always gives you perfect gifts\r
+Perfect amount of physical affection\r
+Adventurous\r
+Loves you unconditionally\r
+In touch with their feelings\r
+Your parents love them\r
+Your friends and enemies are jealous\r
+Owns a magic carpet\r
+Volunteers with elderly people\r
+Loves dogs\r
+Loves cats\r
+Loves theater\r
+Animal shelter volunteer\r
+Travels a lot\r
+Really into wine\r
+Loves video games\r
+Has front row tickets to every concert\r
+Has front row tickets to every sporting event\r
+Loves sports\r
+Loves the outdoors\r
+Loves staying in\r
+Owns your favorite museum\r
+Same favorite movies\r
+Same favorite books\r
+Same favorite foods\r
+Same favorite music\r
+Lives off the grid\r
+Loves children\r
+Millionaire\r
+Most physically attractive person you have ever seen\r
+Owns a vineyard\r
+Owns an adorable coffee shop\r
+Lives on 100 beachfront acres\r
+Owns a solid gold private jet\r
+Has a magic bag that contains unlimited cheese\r
+Owns a 500-foot yacht\r
+Owns an island in the Bahamas\r
+Owns one of every car\r
+Owns 100 horses\r
+Owns your favorite sports team\r
+Owns a penthouse suite in your favorite city\r
+Gives billions to your favorite charity\r
+Owns a time machine\r
+Lives in a castle\r
+Has the best hair ever\r
+Owns a unicorn\r
+Genius-level IQ\r
+Owns a successful startup\r
+Best sex ever\r
+Can get you both into literally anywhere\r
+You'll never have to work while you date them\r
+You'll never get sick while you date them\r
+You literally will not age while you date them\r
+Always smells like _____\r
+Looks exactly like _____\r
+Has a mansion in _____\r
+Speaks every romantic language\r
+Drives a _____\r
+Is a professional _____\r
+Owns a _____\r
+Knows everything about _____\r
+Volunteers for _____\r
+You will never gain weight while you date them\r
+Has the voice of an angel\r
+Owns a goose that lays designer clothing\r
+Has a cellar full of _____\r
+Professional surfer\r
+Rocket scientist\r
+Model\r
+Professional football player\r
+Famous artist\r
+College English professor\r
+Firefighter\r
+Astronaut\r
+President\r
+Rodeo champion\r
+Professional wrestler\r
+Famous movie star\r
+Famous musician\r
+Famous rapper\r
+Porn star\r
+Supermodel\r
+Professional chef\r
+Is never gonna give you up / Let you down / Run around / Or desert you\r
+World's best hair stylist\r
+Brain surgeon\r
+Lawyer\r
+CEO\r
+World's best advertising executive\r
+Airline pilot\r
+Marine\r
+Spin instructor\r
+Bestselling author\r
+YouTube celebrity\r
+Lumberjack\r
+Organic farmer\r
+Stunt double\r
+Park ranger\r
+Ski instructor\r
+Photographer\r
+Famous fashion designer\r
+Is royalty\r
+Is a genie\r
+World champion _____\r
+World champion hunter\r
+Won the medal of honor\r
+Was on the news for rescuing a kitten\r
+Best-smelling person in the world\r
+Yoga instructor\r
+The hot barista\r
+Has an 8-pack\r
+Olympic gymnast\r
+Pick an actor/actress\r
+Never arrives prematurely\r
+Loves to cuddle\r
+Is a vampire... / the sexy kind\r
+Has the most attractive _____ you've ever seen\r
+The lead singer of your favorite band\r
+Beautiful eyes\r
+Completely blind to all of your flaws\r
+Spontaneous and fun\r
+Makes you feel secure\r
+Has the hottest friends\r
+Has a twin\r
+Life is the true story that _____ is based on\r
+Can fly\r
+Superhero\r
+Loves to laugh\r
+Makes scented candles\r
+Loves the '80s\r
+Cooks all your favorite foods\r
+Knows all the words to your favorite children's TV theme songs\r
+Never blames you for anything\r
+Can get you free drinks forever at any bar in the world\r
+Creates shelters for the homeless\r
+Their kisses cure cancer\r
+Is a great conversationalist\r
+Gives the best hugs\r
+Gives you multiple orgasms\r
+Is a merperson\r
+Always knows how to stop you from crying\r
+Can multiply themselves\r
+An amazing dancer\r
+Always defends you\r
+Can get you a table at any 5-star restaurant any time any place\r
+Can fire your most hated co-worker or boss\r
+Their farts smell like flowers\r
+Never farts\r
+Their morning breath smells like fresh-baked cookies\r
+Owns Mars\r
+Is a night person and a morning person\r
+Loves their job\r
+Has all the same hobbies as you\r
+Can talk to animals\r
+Owns real dinosaurs\r
+Gives great massages`,T=`Thinks they're a Jedi\r
+Civil War reenactor\r
+Super super racist\r
+Sexist\r
+Really stupid\r
+Owes the government one million dollars\r
+Death row inmate\r
+Zero percent sexually attractive to you\r
+Thinks you're ugly\r
+Their pet name for you is "trash bag"\r
+Is always covered in ants\r
+Really really hates children\r
+Only speaks in horrible puns\r
+Is a merperson\r
+Sees the ghost of everyone who has ever died\r
+Is literally always watching porn\r
+Is addicted to crack\r
+Is addicted to prescription drugs\r
+Is a train wreck\r
+Backseat driver\r
+Reads over your shoulder (out loud)\r
+Is six years old\r
+Only speaks in sarcasm\r
+Farts every time they kiss you\r
+Kicks every animal they see\r
+Worst-smelling person in the world\r
+Amateur cannibal\r
+Dabbles in torture\r
+Uses the word "chillax" in every sentence\r
+Body type: Dumpster\r
+Has a permanent fedora\r
+Basic\r
+Spends every date using dating apps\r
+Literally can't make decisions\r
+Has to pee every five minutes for five minutes\r
+Sold your pets on Craigslist\r
+Only speaks in yelling\r
+Doesn't know the difference between your and you're\r
+Only speaks in business buzzwords\r
+Eats crayons\r
+Has a dozen kids from a dozen partners\r
+Is wanted in every state except Florida / (Because Florida doesn't want them)\r
+Is a hobbit\r
+Has nineteen kids and counting\r
+Is building an ark\r
+Snorts _____ / Really wants you to try it\r
+Will only eat if you play airplane with their food\r
+Literally always on a Segway\r
+Cries themself to sleep every night\r
+Takes you to _____ on the first date\r
+Is Nickelback\r
+Counts your calories\r
+Can't keep a job longer than twenty minutes\r
+Hair is transplanted from cadaver butts\r
+Looks and smells just like your dad\r
+Throws up every time they see your face\r
+Is your clone\r
+Melts if they get wet\r
+Yawns every time you start a sentence\r
+First name is "Poop"\r
+College nickname: Puke Crotch\r
+Still in fourth grade\r
+Teleports to the North Pole every time they say the word "love"\r
+Every time you think about them you dry heave\r
+Takes you to couples therapy on the first date\r
+Only speaks in boy band lyrics\r
+Hits on every server\r
+Has never spelled a word correctly\r
+Collects human skulls / Says they're "practically free!"\r
+Steals something from every store they enter\r
+Compulsive liar\r
+Is eligible for the Make-A-Wish Foundation\r
+Calls you by exe's name / Every time\r
+Always wearing a Bluetooth headset\r
+Is the person you hated the most in high school / and literally hasn't changed\r
+Posts all of your texts and emails online\r
+3-foot fingernails\r
+Keeps asking if your best friend is "down"\r
+Only communicates with you via fax\r
+Only speaks Klingon\r
+Literally won't stop talking about their mother\r
+Sleep-puncher\r
+Has scissor hands\r
+Describes all food as "yummy yummers"\r
+Writes erotic Lassie fan fiction\r
+Won't allow wi-fi in the house\r
+Refuses to wash hands\r
+Celibate\r
+Has plexiglass cheeks so that you can see inside their mouth\r
+Responds to everything to say with "that's what she said"\r
+Isn't potty trained\r
+Is a serial killer\r
+Only speaks in Disney song lyrics\r
+Still uses a flip phone\r
+Has two other spouses\r
+Only eats deep-fried food\r
+Lives off the grid\r
+Tries to pay for everything with farm animals\r
+Never stops playing the ukulele\r
+Regularly hosts tea parties for dolls\r
+Is the oldest living person\r
+Constantly taking selfies\r
+Has _____ for hands\r
+Says "Can you not" while you cry at a funeral\r
+Adds you as an emergency contact on the first date\r
+Literally only talks about themselves\r
+Only responds to texts with "k"\r
+Is related to you\r
+Incapable of all emotion\r
+Is actually two children stacked on top of each other in a trench coat\r
+Name drops during dirty talk\r
+Tells you to calm down after everything you say\r
+You are allergic to them\r
+Greets people by sniffing their butts\r
+Is totally married\r
+Constantly trying to bang your friends\r
+Refers to their genitals as their "no-no"\r
+Rigged to a time bomb\r
+Is a zombie\r
+Conveniently forgets wallet every date night / Always has ID though\r
+Calls you a "4" to your face\r
+Steals locks of your hair\r
+Their kisses taste like poop / and they love kissing\r
+Repeats your jokes but louder\r
+Compulsively licks doorknobs\r
+Physically incapable of asking a question\r
+Jealous of your pet\r
+Afraid of sunlight\r
+Speaks with a constantly changing foreign accent\r
+Literally always drunk\r
+Brings ex to first date\r
+Brings mom to first date\r
+Burps and blows in your face\r
+In a band called "Genocide Warts"\r
+Literally never stops talking\r
+Constantly plotting to kill you\r
+Open mouth kisses their pets\r
+Immediately tries to kill you with a fork\r
+Super clingy\r
+Open mouth kisses their siblings\r
+Literally won't talk to you if you're part of the wrong Hogwarts house\r
+Greets you with a headbutt\r
+Has slept with literally everyone\r
+Always picking their nose\r
+Only speaks in hashtags\r
+Demands to pop all of your zits for you\r
+Recites all the lines from "Titanic" in their sleep\r
+Narrates everything they do out loud\r
+Puts "air quotes" around "every word" they say\r
+Knocks things out of strangers' hands while walking down the street\r
+Charges you a quarter every time you say a curse word\r
+Is dead\r
+Is literally always naked\r
+Talks dirty to all of their food\r
+Has the names of all of their exes tattooed on their face\r
+Only wears clothing made of human hair\r
+Wears diapers because it's "more convenient"\r
+Can only speak in lines from _____\r
+Only laughs at their own jokes\r
+Keeps changing the subject to talk about how not racist they are\r
+Just got out of prison / but won't tell you what they were in for\r
+50 Shades of Grey is literally the only book they've read\r
+Doesn't think women should vote\r
+Has been stalking you since middle school\r
+Is 40 years older than they were in their profile picture\r
+Is a vampire... / Not the sexy kind\r
+Winks at you after everything they say ;)\r
+Swears you look exactly like their sister / But says "that's a good thing"\r
+Tinders during sex\r
+Wears a backpack full of _____ during sex\r
+Men's rights activist\r
+Is a 60 year old virgin\r
+Ends every conversation with "you're dismissed"\r
+Sells home-made pubic hair wigs\r
+Still really into Myspace\r
+Their sex tape with _____ just went viral\r
+Can only have conversations through puppets\r
+Always wears a luchador mask\r
+Has half a beard / Absolutely refuses to shave\r
+Swears they're eighteen\r
+Yells "ear muffs!" whenever they hear a bad word\r
+Amateur vigilante\r
+Won't stop juggling\r
+Resting dumb face\r
+Keeps talking about their "hot cousin"\r
+Talks exclusively in emojis\r
+Refuses to delete online dating profile\r
+Always bragging about past sexual conquests\r
+Can only eat out of someone else's hand\r
+Uses way way way too much tongue\r
+Refuses to cover face while sneezing\r
+Refuses to look up from phone\r
+Keeps talking about someone who looks just like you / Only better!\r
+Thinks Twilight deserved an Oscar\r
+Is afraid of stoplights\r
+Doesn't understand your jokes\r
+Doesn't believe in oral sex\r
+Laughs incessantly during sex\r
+Is a revenge porn mogul\r
+Live-tweets your entire date\r
+Slept with one of your parents / Recently\r
+For as long as you date them all your friends will hate you\r
+Always dresses like a clown\r
+Constantly freestyles / Badly\r
+Has a _____ fetish\r
+Only speaks in motivational quotes\r
+The two halves of their face don't match / Both are attractive but look totally different\r
+Doesn't know or understand any word that starts with the letter "D"\r
+Only has eyes for you / They literally can't see anyone in the world except you\r
+Has a superpower / It's seeing through people's clothes\r
+Always takes the joke three steps too far\r
+Is on every fad diet at the same time\r
+Calls their pets their children\r
+Static shocks you every time you touch\r
+Is allergic to all of your favorite foods\r
+Says "like", like all the time\r
+Literally has the face of a pug\r
+Begins every sentence with "actually"\r
+Lives in a bedroom with eight dudes / Including one named "Jug"\r
+Doesn't like music... / any of it\r
+Thinks they're smarter than you\r
+As long as you date them you can only eat food off the dollar menu`;const f=r=>r[Math.floor(Math.random()*r.length)],R=A.split(`
+`),B=T.split(`
+`),g=(r,t)=>{const n=Array.from([...Array(t).keys()]);return console.log(n),n.map(()=>{let s=f(r);for(;n.includes(s);)s=f(r);return console.log(s),s})},P=r=>g(R,r),W=r=>g(B,r),p={props:{text:String},setup(r){const t=r;return(n,s)=>(i(),u("div",null,_(t.text),1))}},D={class:"bg-gray-50 flex full-width"},F={class:"text-green-500 w-1/2"},j={class:"text-red-500 w-1/2"},m={setup(r){const t=y([]),n=y([]),s=()=>{n.value=P(2),t.value=W(1)};return s(),(e,o)=>(i(),u(h,null,[l("div",D,[l("div",F,[(i(!0),u(h,null,c(n.value,a=>(i(),d(p,{text:a},null,8,["text"]))),256))]),l("div",j,[(i(!0),u(h,null,c(t.value,a=>(i(),d(p,{text:a},null,8,["text"]))),256))])]),l("button",{class:"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded",onClick:s},"reroll")],64))}},N=[{path:"/",component:m,meta:{title:"Home"}},{path:"/:path(.*)",component:m}],v=b(H),M=I({history:O(),routes:N});v.use(M);v.mount("#app");

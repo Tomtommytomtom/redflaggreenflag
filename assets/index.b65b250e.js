20005bb43388b8610849258b7ac2484145df5be6
Tom Pillokat <tom.pillokat@googlemail.com>
2022-02-20 21:01:06 +0100

    Updates

---
+++ assets/index.b65b250e.js
@@ -0,0 +1,391 @@
+import{r as j,o as r,c as l,a as i,b as u,t as M,F as $,d as A,e as N,f as m,g as h,h as w,i as C,j as P,k as D,l as R}from"./vendor.960219fb.js";const F=function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))o(t);new MutationObserver(t=>{for(const a of t)if(a.type==="childList")for(const c of a.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&o(c)}).observe(document,{childList:!0,subtree:!0});function s(t){const a={};return t.integrity&&(a.integrity=t.integrity),t.referrerpolicy&&(a.referrerPolicy=t.referrerpolicy),t.crossorigin==="use-credentials"?a.credentials="include":t.crossorigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function o(t){if(t.ep)return;t.ep=!0;const a=s(t);fetch(t.href,a)}};F();var _=(n,e)=>{const s=n.__vccOpts||n;for(const[o,t]of e)s[o]=t;return s};const L={},z={class:"h-full"},T={class:"h-full"};function O(n,e){const s=j("router-view");return r(),l("div",z,[i("main",T,[u(s)])])}var H=_(L,[["render",O]]),I=`is fiercely loyal
+believes you are the most attractive person they've ever seen
+is trusting
+is respectful
+is honest
+is patient
+is reliable
+is courteous
+is the funniest person in the world
+is affectionate
+is a good listener
+is supportive
+is generous
+is driven
+is the perfect communicator
+always makes time for you
+always gives you perfect gifts
+always gives the perfect amount of physical affection
+is adventurous
+loves you unconditionally
+is in touch with their feelings
+is great with your parents 
+makes friends and enemies jealous
+owns a magic carpet
+volunteers with elderly people
+loves dogs
+loves cats
+loves theater
+volunteers at an animal shelter
+travels a lot
+is really into wine
+loves video games
+has front row tickets to every concert
+has front row tickets to every sporting event
+loves sports
+loves the outdoors
+loves staying in
+owns your favorite museum
+has the same favourite _____
+lives off the grid
+loves children
+is a millionaire
+is the most physically attractive person you have ever seen
+owns a vineyard
+owns an adorable coffee shop
+lives on 100 beachfront acres
+owns a solid gold private jet
+Hhas a magic bag that contains unlimited cheese
+owns a 500-foot yacht
+owns an island in the Bahamas
+owns one of every car
+owns 100 horses
+owns your favorite sports team
+owns a penthouse suite in your favorite city
+gives billions to your favorite charity
+owns a time machine
+lives in a castle
+has the best hair ever
+owns a unicorn
+is an actual genius
+owns a successful startup
+is the best sex you've ever had
+can get you both into literally anywhere
+has enough money so you don't have to work
+will make you immortal while you date them
+Always smells like _____
+Looks exactly like _____
+has a mansion
+own a castle
+speaks every romantic language
+drives a _____
+is a professional _____
+knows everything about _____
+volunteers for _____
+has the voice of an angel
+owns a goose that lays designer clothing
+has a donkey that poops gold
+has a cellar full of _____
+is a professional surfer
+is a rocket scientist
+is a professional football player
+is a famous artist
+is a college English professor
+is a firefighter
+is an astronaut
+is the President
+is a rodeo champion
+is a professional wrestler
+is a famous movie star
+is a famous musician
+is a famous rapper
+is a porn star
+is a supermodel
+is a professional chef
+is never gonna give you up / let you down / run around / or desert you
+is the world's best hair stylist
+is a brain surgeon
+is a lawyer
+is a CEO
+is the world's best advertising executive
+is an airline pilot
+is in the marines
+is a spin instructor
+is a bestselling author
+is a YouTube celebrity
+is a Lumberjack
+is an rganic farmer
+is a stunt double
+is a park ranger
+is a ski instructor
+is a photographer
+is a famous fashion designer
+is royalty
+is a genie that grants you three wishes
+won the medal of honor
+was on the news for rescuing a kitten
+is the best-smelling person in the world
+is a yoga instructor
+is the hot barista
+has an 8-pack
+has an onlympic gold medal
+never arrives prematurely
+loves to cuddle
+is a vampire. Yes, the sexy kind
+Has the most attractive _____ you've ever seen
+is the lead singer of your favorite band
+has beautiful eyes
+is completely blind to all of your flaws
+is spontaneous and fun
+makes you feel secure
+has the hottest friends
+has a twin
+has contant food catering 
+can fly
+is a superhero
+loves to laugh
+makes scented candles
+loves the '80s
+cooks all your favorite foods
+knows all the words to your favorite children's TV theme songs
+never blames you for anything
+can get you free drinks forever at any bar in the world
+creates shelters for the homeless
+can cure cancer
+is a great conversationalist
+gives the best hugs
+gives you multiple orgasms
+is a merperson
+always knows how to stop you from crying
+can multiply themselves
+is an amazing dancer
+always defends you
+can get you a table at any 5-star restaurant. Any time, any place.
+can fire your most hated co-worker or boss
+their farts smell like flowers
+never farts
+their morning breath smells like fresh-baked cookies
+owns Mars
+is a night person and a morning person
+loves their job
+has all the same hobbies as you
+can talk to animals
+owns real dinosaurs
+gives great massages
+`,q=`thinks they're a Jedi
+plays War
+is super super racist
+is sexist
+is really stupid
+owes the government one million dollars
+is a death row inmate
+is zero percent sexually attractive to you
+thinks you're ugly
+their pet name for you is "trash bag"
+is always covered in ants
+really really hates children
+only speaks in horrible puns
+is a merperson
+sees the ghost of everyone who has ever died
+is literally always watching porn
+is addicted to crack
+is addicted to prescription drugs
+is a train wreck
+is a backseat driver
+reads over your shoulder (out loud)
+is six years old
+only speaks in sarcasm
+farts every time they kiss you
+kicks every animal they see
+is the worst-smelling person in the world
+is an amateur cannibal
+dabbles in torture
+uses the word "chillax" in every sentence
+Body type: Dumpster
+has a permanent fedora
+is basic
+spends every date using dating apps
+literally can't make decisions
+has to pee every five minutes for five minutes
+sold your pets on Craigslist
+only speaks in yelling
+doesn't know the difference between your and you're
+only speaks in business buzzwords
+eats crayons
+has a dozen kids from a dozen partners
+is wanted in every state except Florida. Because Florida doesn't want them.
+is a hobbit
+has nineteen kids and counting
+is building an ark
+snorts pre-workout and really wants you to try it
+will only eat if you play airplane with their food
+is literally always on a Segway
+cries themself to sleep every night
+takes you to _____ on the first date
+is Nickelback
+counts your calories
+can't keep a job longer than twenty minutes
+their hair is transplanted from cadaver butts
+looks and smells just like your dad
+throws up every time they see your face
+is your clone
+melts if they get wet
+yawns every time you start a sentence
+their first name is "Poop"
+has the nickname: _____
+is still in fourth grade
+teleports to the North Pole every time they say the word "love"
+every time you think about them you dry heave
+takes you to couples therapy on the first date
+hits on every server
+has never spelled a word correctly
+collects human skulls and says they're "practically free!"
+steals something from every store they enter
+is a compulsive liar
+is eligible for the Make-A-Wish Foundation
+calls you by exe's name every time
+always wears a Bluetooth headset
+is the person you hated the most in high school and literally hasn't changed
+posts all of your texts and emails online
+has 3-foot fingernails
+keeps asking if your best friend is "down"
+only communicates with you via fax
+only speaks Klingon
+literally won't stop talking about their mother
+is a sleep-puncher
+has scissor hands
+describes all food as "yummy yummers"
+writes erotic Spongebob fan fiction
+won't allow wi-fi in the house
+refuses to wash their hands
+is in celibate
+has plexiglass cheeks so that you can see inside their mouth
+responds to everything to say with "that's what she said"
+isn't potty trained
+is a serial killer
+still uses a flip phone
+has two other spouses
+only eats deep-fried food
+lives off the grid
+tries to negotiate the price of everything
+never stops playing the ukulele
+regularly hosts tea parties for dolls
+is the oldest living person
+constantly takes selfies
+has _____ for hands
+says "Can you not" while you cry at a funeral
+adds you as an emergency contact on the first date
+literally only talks about themselves
+only responds to texts with "k"
+is related to you
+ia incapable of all emotion
+is actually two children stacked on top of each other in a trench coat
+name drops during dirty talk
+tells you to calm down after everything you say
+you are allergic to them
+greets people by sniffing their butts
+is totally married
+constantly tries to bang your friends
+refers to their genitals as their "no-no"
+is rigged to a time bomb
+is a zombie
+conveniently forgets wallet every date night.
+calls you a "4" to your face
+steals locks of your hair
+their kisses taste like poop
+repeats your jokes but louder
+compulsively licks doorknobs
+is physically incapable of asking a question
+is jealous of your pet
+is afraid of sunlight
+speaks with a constantly changing foreign accent
+is always always drunk
+brings ex to first date
+brings mom to first date
+burps and blows in your face
+is in a fried group called "_____"
+never stops talking
+is constantly plotting to kill you
+open mouth kisses their pets
+immediately tries to kill you with a fork
+is super clingy
+open mouth kisses their siblings
+won't talk to you if you're part of the wrong Hogwarts house
+greets you with a headbutt
+has slept with literally everyone you know
+always picks their nose
+only speaks in hashtags
+demands to pop all of your zits for you
+recites all the lines from "Titanic" in their sleep
+narrates everything they do out loud
+puts "air quotes" around "every word" they say
+knocks things out of strangers' hands while walking down the street
+charges you a quarter every time you say a curse word
+is dead
+is always naked
+talks dirty to all of their food
+has the names of all of their exes tattooed on their face
+only wears clothing made of human hair
+wears diapers because it's "more convenient"
+can only speak in lines from _____
+only laughs at their own jokes
+Keeps changing the subject to talk about how not racist they are
+just got out of prison but won't tell you what they were in for
+50 Shades of Grey is the only book they've read
+doesn't think women should vote
+has been stalking you since middle school
+is 40 years older than they were in their profile picture
+is a vampire. And not the sexy kind
+winks at you after everything they say ;)
+swears you look exactly like their siste but says "that's a good thing"
+tinders during sex
+wears a backpack full of _____ during sex
+is a men's rights activist
+is a 60 year old virgin
+ends every conversation with "you're dismissed"
+sells home-made pubic hair wigs
+is still really into Myspace
+their sex tape with _____ just went viral
+can only have conversations through puppets
+always wears a luchador mask
+has half a beard and absolutely refuses to shave
+swears they're eighteen
+yells "ear muffs!" whenever they hear a bad word
+is an amateur _____
+won't stop juggling
+has a resting dumb face
+keeps talking about their "hot cousin"
+talks exclusively in emojis
+refuses to delete online dating profile
+ia always bragging about past sexual conquests
+can only eat out of someone else's hand
+uses way way way too much tongue
+refuses to cover face while sneezing
+refuses to put away phone
+keeps talking about someone who looks just like you. Only better!
+thinks Twilight deserved an Oscar
+is afraid of stoplights
+doesn't understand your jokes
+doesn't believe in oral sex
+laughs incessantly during sex
+is a revenge porn mogul
+live-tweets your entire date
+slept with one of your parents
+for as long as you date them all your friends will hate you
+always dresses like a clown
+constantly beatboxes
+has a _____ fetish
+only speaks in motivational quotes
+the two halves of their face don't match. Both are attractive but look totally different
+doesn't know or understand any word that starts with the letter "D"
+only has eyes for you. They literally can't see anyone in the world except you
+has a superpower. It's seeing through people's clothes
+always takes the joke three steps too far
+is on every diet at the same time
+calls their pets their children
+static shocks you every time you touch
+is allergic to all of your favorite foods
+says "like", like all the time
+literally has the face of a pug
+begins every sentence with "actually"
+lives in a one-bedroom with eight dudes.
+doesn't like any music
+they thinks they're smarter than you
+as long as you date them you can only eat food off the dollar menu
+can't stand animals
+`;const k=n=>n[Math.floor(Math.random()*n.length)],G=I.split(`
+`),Y=q.split(`
+`),S=(n,e)=>{const s=Array.from([...Array(e).keys()]);return console.log(s),s.map(()=>{let o=k(n);for(;s.includes(o);)o=k(n);return console.log(o),o})},V=n=>S(G,n),K=n=>S(Y,n);const Q={class:"box italic"},B={props:{text:String},setup(n){const e=n;return(s,o)=>(r(),l("div",Q,M(e.text),1))}},W=i("span",{class:"italic"},"YOUR DATE",-1),X={key:0,class:"italic"},b={props:{perks:Array},setup(n){return(e,s)=>(r(),l("div",null,[W,(r(!0),l($,null,A(n.perks,(o,t)=>(r(),l("div",null,[u(B,{text:o,id:`${t}-perk`},null,8,["text","id"]),t!==n.perks.length-1?(r(),l("span",X,"AND")):N("",!0)]))),256))]))}},U=i("span",{class:"italic"},"(BUT)",-1),J={key:0,class:"italic"},Z={props:{negatives:Array},setup(n){return(e,s)=>(r(),l("div",null,[U,(r(!0),l($,null,A(n.negatives,(o,t)=>(r(),l("div",null,[u(B,{text:o,id:`${t}-negative`},null,8,["text","id"]),t!==n.negatives.length-1?(r(),l("span",J,"AND")):N("",!0)]))),256))]))}};const ee={},ne={class:"header italic"},te=i("div",null,[i("span",{class:"font-bold"},"RED"),m(" FLAGS")],-1),se=i("div",null,[i("span",{class:"font-bold"},"GREEN"),m(" FLAGS")],-1),oe=[te,se];function ae(n,e){return r(),l("div",ne,oe)}var ie=_(ee,[["render",ae]]);const re={},le={class:"footer italic flex"},he=i("div",{class:"grow"},[i("div",null,[i("span",{class:"font-bold"},"RED"),m(" FLAGS")]),i("div",null,[i("span",{class:"font-bold"},"GREEN"),m(" FLAGS")])],-1),ue=i("div",null,"icon",-1),ce=[he,ue];function de(n,e){return r(),l("div",le,ce)}var ye=_(re,[["render",de]]);const pe=()=>{const n=document.getElementById("outer-box").getBoundingClientRect(),e=document.getElementById("0-perk").getBoundingClientRect(),s=document.getElementById("0-negative").getBoundingClientRect();console.log(e),console.log(s);const o={x:Math.round(e.right-50),y:Math.round(e.top)},t={x:Math.round(s.right-80),y:Math.round(s.top)},a={x:Math.round(n.left),y:Math.round(o.y-120)},c={x:Math.round(n.right),y:Math.round(t.y-80)};console.log(o),console.log(t);const y=document.getElementById("svg-overlay"),p=document.createElementNS("http://www.w3.org/2000/svg","path");p.setAttributeNS(null,"id","first-path"),p.setAttributeNS(null,"d",`M ${a.x} ${a.y} Q ${a.x+100} ${a.y} ${o.x} ${o.y}`),p.setAttributeNS(null,"style","fill:none;stroke:#000000;stroke-width:1px;opacity:0;"),y.appendChild(p);const f=document.createElementNS("http://www.w3.org/2000/svg","path");f.setAttributeNS(null,"id","second-path"),f.setAttributeNS(null,"d",`M ${o.x} ${o.y} Q ${o.x+100} ${o.y-200} ${t.x} ${t.y}`),f.setAttributeNS(null,"style","fill:none;stroke:#000000;stroke-width:1px;opacity:0;"),y.appendChild(f);const g=document.createElementNS("http://www.w3.org/2000/svg","path");g.setAttributeNS(null,"id","third-path"),g.setAttributeNS(null,"d",`M ${t.x} ${t.y} Q ${t.x+40} ${t.y-100} ${c.x} ${c.y}`),g.setAttributeNS(null,"style","fill:none;stroke:#000000;stroke-width:1px;opacity:0;"),y.appendChild(g);const d=document.createElementNS("http://www.w3.org/2000/svg","circle");d.setAttributeNS(null,"cx",0),d.setAttributeNS(null,"cy",0),d.setAttributeNS(null,"class","ball"),d.setAttributeNS(null,"r","5px"),d.setAttributeNS(null,"style","fill:#000000;opacity:0;"),y.appendChild(d)},v=n=>{const e=document.querySelector(n);console.log(e),e.style.opacity=1},fe=n=>{v(".ball"),h({targets:"#first-path",strokeDashoffset:[h.setDashoffset,0],easing:"easeInCubic",complete:n,duration:1500});const e=h.path("#first-path");console.log(e("x")),h({targets:".ball",translateX:e("x"),translateY:e("y"),easing:"easeInCubic",angle:e("angle"),duration:1500}),v("#first-path")},ge=n=>{const e="cubicBezier(0.010, 0.25, 0.70, 0.010)";h({targets:"#second-path",strokeDashoffset:[h.setDashoffset,0],easing:e,complete:n,duration:1500,delay:function(o,t){return t*250}});const s=h.path("#second-path");console.log(s("x")),h({targets:".ball",translateX:s("x"),translateY:s("y"),easing:e,angle:s("angle"),duration:1500}),v("#second-path")},me=()=>{const n="easeOutCubic";h({targets:"#third-path",strokeDashoffset:[h.setDashoffset,0],easing:n,duration:400,delay:function(s,o){return o*250}});const e=h.path("#third-path");console.log(e("x")),h({targets:".ball",translateX:e("x"),translateY:e("y"),easing:n,angle:e("angle"),duration:400}),v("#third-path")},ve=()=>{fe(()=>ge(me))};const _e={class:"h-full p-2"},we={id:"outer-box",class:"outer-box w-full p-2"},ke={class:"grow-bottom-container"},be={class:"flex inner-box"},xe={class:"w-1/2"},x={setup(n){const e=w([]),s=w([]);return(()=>{s.value=V(2),e.value=K(1),e.value[0]=`The two halves of their
+face don\u2018t match /
+Both are attractive but
+look totally different`})(),C(()=>{pe(),ve()}),(t,a)=>(r(),l("div",_e,[i("div",we,[u(ie),i("div",ke,[i("div",be,[u(b,{class:"w-1/2",perks:s.value},null,8,["perks"]),i("div",xe,[u(b,{style:{visibility:"hidden"},perks:s.value},null,8,["perks"]),u(Z,{negatives:e.value},null,8,["negatives"])])])]),u(ye,{class:"mt-16"})])]))}},$e=[{path:"/",component:x,meta:{title:"Home"}},{path:"/:path(.*)",component:x}],E=P(H),Ae=D({history:R(),routes:$e});E.use(Ae);E.mount("#app");

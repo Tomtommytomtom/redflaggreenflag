fd0199a2e8de350ccaf39150fe93fe0eeb8e298c
Tom Pillokat <tom.pillokat@googlemail.com>
2022-02-22 15:00:15 +0100

    new blanks data

---
+++ src/assets/just_blanks.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const negatives = fs.readFileSync('./NEGATIVES.txt').toString().trim().split('\n')
+const positives = fs.readFileSync('./PERKS.txt').toString().trim().split('\n')
+
+const filteredPositives = positives.filter(s => !s.includes("____"))
+const filteredNegatives = negatives.filter(s => !s.includes("____"))
+
+fs.writeFileSync("./NO-BLANKS-POSITIVES.txt",filteredPositives.sort().join("\n").trim());
+fs.writeFileSync("./NO-BLANKS-NEGATIVES.txt",filteredNegatives.sort().join("\n").trim());
